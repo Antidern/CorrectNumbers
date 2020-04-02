fa0a0d34f6e55bf0e3c88c3a1a4a1460ea062e55
Antidern <dmitidern@mail.ru>
2020-04-02 23:29:43 +0300

    Отображение текста файла на веб-странице

--- numbers.js
+++ numbers.js
@@ -1,1 +1,11 @@
-
+var test;
+$.ajax({
+    url: "test.txt",
+    dataType: "text",
+    async: true,
+    success: function(msg){
+        test = msg;
+        // alert('Содержимое файла: '+test);
+        document.getElementById("content").innerHTML = (test);
+    }
+});
